b52250d5554d1f4b903d81e1dfc667137e9b4eff
Tim Jacomb <tim.jacomb@hmcts.net>
2022-08-24 11:39:21 +0100

    Productionise

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 PRODUCT=$(rg --no-line-number 'product = "(.+)"' Jenkinsfile_CNP -or '$1')
+export PRODUCT
+
 COMPONENT=$(rg --no-line-number 'component = "(.+)"' Jenkinsfile_CNP -or '$1')
 
 APP_NAME="${PRODUCT}-${COMPONENT}"
@@ -21,6 +23,32 @@ else
   JENKINS_FOLDER_NAME=HMCTS_j_to_z
 fi
 
+LANGUAGE=$(rg 'java' Jenkinsfile_CNP)
+
+if rg 'java' Jenkinsfile_CNP > /dev/null
+then
+  LANGUAGE=java
+else
+  LANGUAGE=nodejs
+fi
+
+if echo "$REPOSITORY" | grep shared-infra
+then
+  APP_NAME="${PRODUCT}-shared-infrastructure"
+  LANGUAGE=terraform
+fi
+
+JENKINS_INSTANCE=cft
+
+if [ ! -f /tmp/team-config.yml ]
+then
+  wget https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/team-config.yml -O /tmp/team-config.yml
+fi
+
+SLACK_CHANNEL=$(yq '.[env(PRODUCT)].slack.contact_channel' /tmp/team-config.yml)
+
+OWNER=$(yq '.[env(PRODUCT)].azure_ad_group | downcase | sub(" ", "_")' /tmp/team-config.yml)
+
 cat << EOF > catalog-info.yaml
 apiVersion: backstage.io/v1alpha1
 kind: Component
@@ -28,18 +56,18 @@ metadata:
   name: "${APP_NAME}"
   annotations:
     # This must match folder-name/job-name in Jenkins.
-    jenkins.io/job-full-name: cft:$JENKINS_FOLDER_NAME/$REPO_NAME_ONLY
+    jenkins.io/job-full-name: $JENKINS_INSTANCE:$JENKINS_FOLDER_NAME/$REPO_NAME_ONLY
     github.com/project-slug: $REPOSITORY
   tags:
-    - java
+    - ${LANGUAGE}
   links:
     - url: https://hmcts-reform.slack.com/app_redirect?channel=platops-help
-      title: "#platops-help on Slack"
+      title: "${SLACK_CHANNEL} on Slack"
       icon: chat
 spec:
   type: service
   lifecycle: production
-  owner: dts_platform_operations
+  owner: $OWNER
 EOF
 
-cat catalog-info.yaml
+#cat catalog-info.yaml
